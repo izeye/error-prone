b6d6b0bb752768ef9aa8fc31feb0ed94aff1d228
Liam Miller-Cushon <cushon@google.com>
2025-04-14 11:49:16 -0700

    Handle locals in InlineFormatString
    PiperOrigin-RevId: 747507380

--- core/src/test/java/com/google/errorprone/bugpatterns/formatstring/InlineFormatStringTest.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/formatstring/InlineFormatStringTest.java
@@ -206,4 +206,47 @@ public class InlineFormatStringTest {
             """)
         .doTest();
   }
+
+  @Test
+  public void negativeLocal() {
+    compilationHelper
+        .addSourceLines(
+            "Test.java",
+            """
+            class Test {
+              void f(String a) {
+                String format = a;
+                System.err.printf(format, 42);
+              }
+            }
+            """)
+        .doTest();
+  }
+
+  @Test
+  public void refactoringLocal() {
+    refactoringHelper
+        .addInputLines(
+            "Test.java",
+            """
+            class Test {
+              void f() {
+                String format = "hello %s";
+                System.err.printf(format, 42);
+                System.err.printf(format, 42);
+              }
+            }
+            """)
+        .addOutputLines(
+            "Test.java",
+            """
+            class Test {
+              void f() {
+                System.err.printf("hello %s", 42);
+                System.err.printf("hello %s", 42);
+              }
+            }
+            """)
+        .doTest();
+  }
 }
